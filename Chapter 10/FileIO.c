8a526c154ddcb492cc38a98d03311fb4c9ec19d8
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-02-04 21:24:19 +0530

    Chapter 10 Completed

--- Chapter 10/FileIO.c
+++ Chapter 10/FileIO.c
@@ -14,16 +14,52 @@
   
 // File Pointer : FILE is a hidden structure that needs to be created for opening a file A FILE ptr that points to this structure & is used to access the file. (FILE *fter)
 
-//  "r"  --> open to read.
+//  "r"  --> open to read.  
 //  "rb" --> open to read in binary.
 //  "w"  --> open to write.
 //  "wb" --> open to write in binary.
 //  "a"  --> open to append.
 
+// #include<stdio.h>
+// int main(){
+//     FILE *fptr,*hptr;
+//     fptr=fopen("textDemo.txt","w");  
+    // fptr=fopen("textDemo.txt","r");  
+    // hptr=fopen("hello.txt","w");  Creates a file if it is doesn't exist
+    // fputc('s',fptr);
+    // fputc('a',fptr);
+    // fputc('u',fptr);
+    // fputc('r',fptr);
+    // fputc('b',fptr);
+    // fputc('h',fptr);
+    // fputc('c',fptr);
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    // printf("%c",fgetc(fptr));
+    
+    
+//     fclose(fptr);
+// }
+
+// EOF: End Of File.
+// fgetc returns EOF to show that file has ended.
 #include<stdio.h>
 int main(){
     FILE *fptr;
-    fptr=fopen("textDemo.txt","r");  
-    fptr=fopen("textDemo.txt","w");  
+    fptr=fopen("textDemo.txt","r");
+    char ch = fgetc(fptr);
+    while(ch!=EOF){
+        printf("%c",ch);
+        ch=fgetc(fptr);
+    }
 }
-
--- Chapter 10/Program_10.c
+++ Chapter 10/Program_10.c
@@ -13,36 +13,94 @@
 // }
 
 // Read data from file.
-#include<stdio.h>
-int main(){
-    FILE *fptr;
+// #include<stdio.h>
+// int main(){
+//     FILE *fptr;
+//     FILE *fint;
     // Read a file
-    char ch;
-    fptr=fopen("textDemo.txt","r");
-    // fscanf(fptr,"%c",&ch);
-    // printf("character = %c\n",ch);
-    // fscanf(fptr,"%c",&ch);
-    // printf("character = %c\n",ch);
-    // fscanf(fptr,"%c",&ch);
-    // printf("character = %c\n",ch);
-    // fscanf(fptr,"%c",&ch);
-    // printf("character = %c\n",ch);
-    // fscanf(fptr,"%c",&ch);
-    // printf("character = %c\n",ch);
-    
-    // Write in File
+//     char ch;
+//     int a;
+
+//     fptr=fopen("textDemo.txt","r");
+//     fint=fopen("integer.txt","r");
+
+//    for(int i=0;i<7;i++){
+//      fscanf(fptr,"%c",&ch);
+//      printf("%c",ch);
+//    }
+//    printf("\n");
+//    for(int i=0;i<2;i++){
+//     fscanf(fint,"%d",&a);
+//     printf("%d\n",a);
+//    }
+    // Write a file : Erases the data present in a file and write a new data
     // fptr=fopen("textDemo.txt","w");
-    // fprintf(fptr,"%c\n",'S');
-    // fprintf(fptr,"%c\n",'A');
-    // fprintf(fptr,"%c\n",'U');
-    // fprintf(fptr,"%c\n",'R');
-    // fprintf(fptr,"%c\n",'A');
-    // fprintf(fptr,"%c\n",'B');
-    // fprintf(fptr,"%c\n",'H');
+    // fprintf(fptr,"%c",'M');
+    // fprintf(fptr,"%c",'A');
+    // fprintf(fptr,"%c",'N');
+    // fprintf(fptr,"%c",'G');
+    // fprintf(fptr,"%c",'O');
 
-    // Read & Write a char
-    // printf("%c\n",fgetc(fptr));
-    fputc('L',fptr);
+    // Appending a data
+    // fptr=fopen("textDemo.txt","a");
+    // fprintf(fptr,"%c",'M');
+    // fprintf(fptr,"%c",'A');
+    // fprintf(fptr,"%c",'N');
+    // fprintf(fptr,"%c",'G');
+    // fprintf(fptr,"%c",'O');
+// }
 
-    fclose(fptr);
-}
+// Practice Qs 61
+// Make a program to read 5 integers from a file
+// #include<stdio.h>
+// int main(){
+//     FILE *fptr;
+//     fptr=fopen("integer.txt","r");
+//     int a;
+//     for(int i=0;i<5;i++){
+//         fscanf(fptr,"%d",&a);
+//         printf("%d",a);
+//     }
+// }
+
+// Practice Qs 62
+// // Make a program to input student infoormation from a user & enter it to a file.
+// #include<stdio.h>
+// int main(){
+//     FILE *fptr;
+//     fptr = fopen("textDemo.txt","w");
+//     char name[100];
+//     int roll;
+//     float cgpa;
+
+//     printf("Enter Your Name : ");
+//     scanf("%s",name);
+//     printf("Enter Your Roll No. : ");
+//     scanf("%d",&roll);
+//     printf("Enter Your Name : ");
+//     scanf("%.2f",&cgpa);
+
+//     fprintf(fptr,"Name : %s\n",name);
+//     fprintf(fptr,"Roll No. :%d\n",roll);
+//     fprintf(fptr,"CGPA: %.2f\n",cgpa);
+// }
+
+// Practice Qs 63
+// Write a program to write all the odd numbers from 1 to n in the file.
+// #include<stdio.h>
+// int main(){
+//     FILE *fptr;
+//     fptr=fopen("integer.txt","w");
+//     int u;
+//     printf("Enter a Number : ");
+//     scanf("%d",&u);
+
+//     for(int i=0;i<=u;i++){
+//         if(i%2!=0){
+//             fprintf(fptr,"%d\n",i);
+//         }
+//     }
+// }
+
+// Practice Qs 64
+// 2 numbers - a & b, are written in a file. write a program to replace them with their sum
